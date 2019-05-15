b35e21728af7534cb0eaf93e172797a5e2b46d29
Drimaro <Drimaro@gmail.com>
2019-05-15 22:23:05 +0300

    IDE notifications refactoring

--- Api_mod.py
+++ Api_mod.py
@@ -2,97 +2,100 @@ import requests
 import json
 import time
 
+
 # fetching Jwt for admin admin
-def get_jwt(IP_PORT,login,password):
+def get_jwt(ip_port, username, password):
 
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     }
 
-    #data = '{ \\ \n   "password": "admin",\\ \n   "username": "admin" \\ \n }'
-    data = '{ \n   "password": ' + str(password)+', \n   "username": ' + str(login) + ' \n }'
-    #print(data)
+    # data = '{ \\ \n   "password": "admin",\\ \n   "username": "admin" \\ \n }'
+    data = '{ \n   "password": ' + str(password)+', \n   "username": ' + str(username) + ' \n }'
+    # print(data)
     
-
-
-
     try:
-        response = requests.post('http://'+IP_PORT+'/api/internal/login', headers=headers, data=data)
+        response = requests.post('http://' + ip_port + '/api/internal/login', headers=headers, data=data)
         r = response.text
         
-        
-        j = r.split('"jwt":',1)[1]
-        jwt = j.split('}',1)[0]
+        j = r.split('"jwt":', 1)[1]
+        jwt = j.split('}', 1)[0]
         jwt = jwt.replace('"', "")
         print('we got authentification key')
         
-        return(jwt)
+        return jwt
     except Exception as ex:
         print(ex)
 # fetching AppID and AppName
-def get_APPs(j_token,IP_PORT):
-    MES = 'Bearer ' + j_token
+
+
+def get_apps(j_token, ip_port):
+    mes = 'Bearer ' + j_token
    
     headers = {
         'Accept': 'application/json',
-        'Grpc-Metadata-Authorization': MES,
+        'Grpc-Metadata-Authorization': mes,
     }
 
     params = (
         ('limit', '15000'),
     )
 
-    response = requests.get('http://' + IP_PORT + '/api/applications', headers=headers, params=params)
+    response = requests.get('http://' + ip_port + '/api/applications', headers=headers, params=params)
     string = response.text
-    j = string#.decode('utf-8')
+    j = string  # .decode('utf-8')
     count = int(json.loads(j)['totalCount'])
     for i in range(count):
-        App_ID = json.loads(j)['result'][i]['id']
-        App_Name = json.loads(j)['result'][i]['name']
-        print('App_ID: ' + str(App_ID) + '\nApp_Name: ' + str(App_Name))
-        get_devEUI(j_token,IP_PORT, App_ID)#adding searching for app_id devEui func
-##use it in get APP func        
-def get_devEUI(j_token,IP_PORT,A_ID):
-    MES = 'Bearer ' + j_token
+        app_id = json.loads(j)['result'][i]['id']
+        app_name = json.loads(j)['result'][i]['name']
+        print('app_id: ' + str(app_id) + '\nApp_Name: ' + str(app_name))
+        get_dev_eui(j_token, ip_port, app_id)  # adding searching for app_id devEui func
+
+
+# use it in get APP func
+def get_dev_eui(j_token, ip_port, a_id):
+    mes = 'Bearer ' + j_token
     headers = {
-    'Accept': 'application/json',
-    'Grpc-Metadata-Authorization': MES,
+        'Accept': 'application/json',
+        'Grpc-Metadata-Authorization': mes,
     }
 
     params = (
         ('limit', '15000'),
-        ('applicationID', A_ID),
+        ('applicationID', a_id),
     )
 
-    response = requests.get('http://' + IP_PORT + '/api/devices', headers=headers, params=params)
+    response = requests.get('http://' + ip_port + '/api/devices', headers=headers, params=params)
     j = response.text
     print('Devices we got:')
     count = int(json.loads(j)['totalCount'])
     for i in range(count):
-        devName = json.loads(j)['result'][i]['name']
-        devEUI = json.loads(j)['result'][i]['devEUI']
-        lastSeen = json.loads(j)['result'][i]['lastSeenAt']
+        dev_name = json.loads(j)['result'][i]['name']
+        dev_eui = json.loads(j)['result'][i]['devEUI']
+        last_seen = json.loads(j)['result'][i]['lastSeenAt']
         
-        print(str(i+1) + '\nname: ' + str(devName) + '  devEUI: ' + str(devEUI) + ' lastSeen: ' + str(lastSeen))
-        devaddr = get_devAddr(j_token,IP_PORT,devEUI)
+        print(str(i+1) + '\nname: ' + str(dev_name) + '  devEUI: ' + str(dev_eui) + ' lastSeen: ' + str(last_seen))
+        get_dev_addr(j_token, ip_port, dev_eui)
 
-def get_devAddr(j_token,IP_PORT,devEUI):
-    MES = 'Bearer ' + j_token
+
+def get_dev_addr(j_token, ip_port, dev_eui):
+    mes = 'Bearer ' + j_token
     headers = {
-    'Accept': 'application/json',
-    'Grpc-Metadata-Authorization': MES,
+        'Accept': 'application/json',
+        'Grpc-Metadata-Authorization': mes,
     }
 
-    response = requests.get('http://' +IP_PORT +'/api/devices/'+str(devEUI)+'/activation', headers=headers)
+    response = requests.get('http://' + ip_port + '/api/devices/'+str(dev_eui)+'/activation', headers=headers)
     j = response.text
-    #devActiv = str(json.loads(j)['deviceActivation'])
-    #for el in devActiv:
-    #    prin
-    devAddr = json.loads(j)['deviceActivation']['devAddr']
-    Appskey = json.loads(j)['deviceActivation']['appSKey']
-    Nwskey = json.loads(j)['deviceActivation']['nwkSEncKey']
-    print('devAddr: '+ str(devAddr) + ' Appskey: ' + str(Appskey) + ' Nwskey: ' + str(Nwskey))
+    # dev_activation = str(json.loads(j)['deviceActivation'])
+    # for el in dev_activation:
+    #    print
+    dev_addr = json.loads(j)['deviceActivation']['devAddr']
+    apps_key = json.loads(j)['deviceActivation']['appSKey']
+    nws_key = json.loads(j)['deviceActivation']['nwkSEncKey']
+    print('devAddr: ' + str(dev_addr) + ' Appskey: ' + str(apps_key) + ' Nwskey: ' + str(nws_key))
+
 
 if __name__ == "__main__":
     
@@ -100,10 +103,10 @@ if __name__ == "__main__":
     login = '"admin"'
     password = '"admin"'
     while True:
-        jwt = get_jwt(iport,login,password)
-        get_APPs(jwt,iport)
+        jwt = get_jwt(iport, login, password)
+        get_apps(jwt, iport)
         time.sleep(3600)
-    #js = '{"jwt":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJsb3JhLWFwcC1zZXJ2ZXIiLCJleHAiOjE1NTQ5MTE1OTUsImlzcyI6ImxvcmEtYXBwLXNlcnZlciIsIm5iZiI6MTU1NDgyNTE5NSwic3ViIjoidXNlciIsInVzZXJuYW1lIjoiYWRtaW4ifQ.sf_6kSnU2aWjzCumuIpFswXk5Lc_gM-nYWnqNno3Y2Q"}'
-    #j = js.split('"jwt":',1)[1]
-    #j = j.split('}',1)[0]
-    #print(j)
+    # js = '{"jwt":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJsb3JhLWFwcC1zZXJ2ZXIiLCJleHAiOjE1NTQ5MTE1OTUsImlzcyI6ImxvcmEtYXBwLXNlcnZlciIsIm5iZiI6MTU1NDgyNTE5NSwic3ViIjoidXNlciIsInVzZXJuYW1lIjoiYWRtaW4ifQ.sf_6kSnU2aWjzCumuIpFswXk5Lc_gM-nYWnqNno3Y2Q"}'
+    # j = js.split('"jwt":',1)[1]
+    # j = j.split('}',1)[0]
+    # print(j)
--- SERVER.py
+++ SERVER.py
@@ -1,43 +1,47 @@
-from socketserver import ThreadingMixIn
-from http.server import BaseHTTPRequestHandler, HTTPServer
-#from urllib.parse import parse_qs
+import base64
+import binascii
+# from urllib.parse import parse_qs
 import json
-import binascii,base64
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
 
 
 class ThreadingMULTIServer(ThreadingMixIn, HTTPServer):
     pass
 
-class myHandler(BaseHTTPRequestHandler):
+
+class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
         message = "Server working!"
         self.wfile.write(bytes(message, "utf8"))
         print('зашел пользователь на страничку')
-	#return
-	    
+    # return
+
     def do_POST(self):
-	#self._set_headers()
-        print("PACKET RECIEVED")
+        # self._set_headers()
+        print("PACKET RECEIVED")
         self.data_string = self.rfile.read(int(self.headers['Content-Length']))
-        #print(self.data_string)
+        # print(self.data_string)
         self.send_response(200)
         self.end_headers()
         data = self.data_string
-        data = jreader.json_read(data)
+        data = Jreader.json_read(data)
         print(data)
         '''
-	readings_dec = binascii.hexlify(base64.b64decode(str(readings[0]))).upper()
-	print('readings data:'+ str(readings))
-	print('decoded_readings: ' + str(readings_dec.decode('utf-8')))
-	#self.wfile.write(bytes("Hello World".encode("UTF-8")))
-	'''        
-	#return
-class jreader():
-    def json_read(a):
-        jstring = a.decode('utf-8')
+    readings_dec = binascii.hexlify(base64.b64decode(str(readings[0]))).upper()
+    print('readings data:'+ str(readings))
+    print('decoded_readings: ' + str(readings_dec.decode('utf-8')))
+    #self.wfile.write(bytes("Hello World".encode("UTF-8")))
+    '''
+    # return
+
+
+class Jreader:
+    def json_read(self):
+        jstring = self.decode('utf-8')
         jjq_GW = json.loads(jstring)['rxInfo'][0]['gatewayID']
         jjq_App = json.loads(jstring)['applicationName']
         jjq_type = json.loads(jstring)['deviceName']
@@ -57,7 +61,7 @@ class jreader():
             jjq_data = jjq_data.decode('utf-8')
             fin = 'неизвестный пакет' + str(jjq_data)
             if jjq_data.startswith('11'):
-                print('recieved zenner payload sp1')
+                print('received zenner payload sp1')
                 a1 = jjq_data[8:10]
                 a2 = jjq_data[6:8]
                 a3 = jjq_data[4:6]
@@ -66,13 +70,14 @@ class jreader():
                 jjq_data = str(int(a,16)) + ' liters'
             else:
                 jjq_data = fin
-        concl = 'GATEWAY_ID: '+ str(jjq_GW) + '\nApplication_Name: '+ str(jjq_App) + '\nDev_Type: '+ str(jjq_type) + '\nDev_EUI: '+ str(jjq_deveui) + '\nData: '+ str(jjq_data)
+        concl = 'GATEWAY_ID: ' + str(jjq_GW) + '\nApplication_Name: ' + str(jjq_App) + '\nDev_Type: ' + str(jjq_type) \
+                + '\nDev_EUI: ' + str(jjq_deveui) + '\nData: ' + str(jjq_data)
         return concl
 
 
-#26 01 1d 9d
-#11 34 13 4f a2 84 14 9d bc d3 8c 5a 56 e9 00 32
-#2f b5 fd 86 f9 85 bc 7a 2b a3 db 46 62 a3 aa 71
-server = ThreadingMULTIServer(('192.168.1.104', 8009), myHandler)
+# 26 01 1d 9d
+# 11 34 13 4f a2 84 14 9d bc d3 8c 5a 56 e9 00 32
+# 2f b5 fd 86 f9 85 bc 7a 2b a3 db 46 62 a3 aa 71
+server = ThreadingMULTIServer(('192.168.1.104', 8009), MyHandler)
 server.serve_forever()
 
